97b9037b50dcf95834290c590a3a745478bd468d
sickk <jaakmyr@gmail.com>
2024-09-23 23:42:12 -0700

    25%

--- src/components/WordleGame.jsx
+++ src/components/WordleGame.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import '../css/wordlegame.css'
 
 function WordleGame() {
-    const [guesses, setGuesses] = useState(Array.from(Array(6), () => new Array(5).fill(' ')))
+    const [guesses, setGuesses] = useState(Array.from(Array(6), () => ''))
     const [currentIndex, setCurrentIndex] = useState(0);
     const secretWord = 'NINJA'
 
@@ -12,23 +12,36 @@ function WordleGame() {
         return () => {
             document.removeEventListener('keydown', detectKeyDown)
         }
-    }, [])
+    }, [guesses])
 
     const detectKeyDown = (event) => {
-        const key = event.key;
-        // if(key === '')
-        console.log(key)
+        const currentGuess = guesses[currentIndex];
+
+        const newGuesses = [...guesses];
+        if(event.key === 'Backspace'){
+            newGuesses[currentIndex] = currentGuess.substring(0, currentGuess.length-1);
+        } else if(/^[a-zA-Z]$/.test(event.key) && currentGuess.length < 5){
+            newGuesses[currentIndex] += event.key.toUpperCase();
+        } else if(event.key === 'Enter' && currentGuess.length === 5){
+            submitMessage()
+        }
+        setGuesses(newGuesses)
+    }
+
+    const submitMessage = () => {
+        if(currentIndex >= 6) return;
+        setCurrentIndex(currentIndex+1);
     }
     
   return (
     <section className="wordlegame">
-        {guesses.map((row, i) => {
+        {guesses.map((word, i) => {
             return (
             <div className="wordlegame--div" key={i}>
-                {row.map((letter, j) => {
+                {Array(secretWord.length).fill('').map((_, j) => {
                     return(
                     <div className="wordlegame--tile" key={j}>
-                        <p>{letter}</p>
+                        <p>{word[j]}</p>
                     </div>
                     )
                 })}
